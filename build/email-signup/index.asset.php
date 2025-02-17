f72e88764b912745d83412ae1ff6c52be9cb45d0
Ross Findlay <r.findlay.05@aberdeen.ac.uk>
2025-02-17 19:28:36 +0100

    Stop tracking build directory

--- build/email-signup/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'c0109f2931d97f7436ae');
--- build/email-signup/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '244999b02de104a8f4b1');
